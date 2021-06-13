594ac2ac7962dc0f4631f4bac2c1966008114a85
Rishabh Sharma <41268157+rishabh-327@users.noreply.github.com>
2021-06-13 21:28:03 +0530

    App file cleanup. Moved content to Home component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { Container, Row, Col } from 'react-bootstrap'
-import classnames from 'classnames'
+import { Container } from 'react-bootstrap'
 
 import styles from './App.module.scss'
 
 import AppHeader from './components/AppHeader'
 import AppFooter from './components/AppFooter'
-import AppSheet from './components/AppSheet'
-import AppSectionTitle from './components/AppSectionTitle'
-
-import DistrictSearchForm from './components/DistrictSearchForm'
-import PincodeSearchForm from './components/PincodeSearchForm'
+import Home from './views/Home'
 
 function App() {
   return (
@@ -18,46 +13,7 @@ function App() {
 
       <main className={styles.appContent}>
         <Container>
-          <Row className="justify-content-center py-md-5">
-            <Col lg="10">
-              <AppSheet className="py-md-5">
-                <Row>
-                  <Col
-                    className={classnames(
-                      styles.firstForm,
-                      'px-0 px-md-3 pb-3 pb-md-0'
-                    )}
-                    md="6"
-                  >
-                    <Row className="justify-content-center">
-                      <Col xs="11" sm="10" lg="8">
-                        <div className="d-flex flex-column justify-content-between py-md-4">
-                          <AppSectionTitle
-                            className="mb-4"
-                            title="Search By District"
-                          />
-                          <DistrictSearchForm />
-                        </div>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col className="px-0 px-md-3 pt-3 pt-md-0" md="6">
-                    <Row className="justify-content-center h-100">
-                      <Col xs="11" sm="10" lg="8">
-                        <div className="d-flex flex-column justify-content-between h-100 py-md-4">
-                          <AppSectionTitle
-                            className="mb-4"
-                            title="Search By Pincode"
-                          />
-                          <PincodeSearchForm />
-                        </div>
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-              </AppSheet>
-            </Col>
-          </Row>
+          <Home />
         </Container>
       </main>
 
---
+++ src/views/Home/index.jsx
@@ -0,0 +1,59 @@
+import { Row, Col } from 'react-bootstrap'
+import classnames from 'classnames'
+
+import './Home.scss'
+import _s from './Home.module.scss'
+
+import AppSheet from '../../components/AppSheet'
+import AppSectionTitle from '../../components/AppSectionTitle'
+import DistrictSearchForm from '../../components/DistrictSearchForm'
+import PincodeSearchForm from '../../components/PincodeSearchForm'
+
+const Home = () => {
+  return (
+    <section id="home-page">
+      <Row className="justify-content-center py-md-5">
+        <Col lg="10">
+          <AppSheet className="py-md-5">
+            <Row className="mx-0">
+              <Col
+                className={classnames(
+                  _s.firstForm,
+                  'px-0 px-md-3 py-3 py-md-0'
+                )}
+                md="6"
+              >
+                <Row className="justify-content-center">
+                  <Col className="px-0" xs="11" sm="10" lg="8">
+                    <div className="d-flex flex-column justify-content-between py-md-4">
+                      <AppSectionTitle
+                        className="mb-4"
+                        title="Search By District"
+                      />
+                      <DistrictSearchForm />
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+              <Col className="px-0 px-md-3 py-3 py-md-0" md="6">
+                <Row className="justify-content-center h-100">
+                  <Col className="px-0" xs="11" sm="10" lg="8">
+                    <div className="d-flex flex-column justify-content-between h-100 py-md-4">
+                      <AppSectionTitle
+                        className="mb-4"
+                        title="Search By Pincode"
+                      />
+                      <PincodeSearchForm />
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </AppSheet>
+        </Col>
+      </Row>
+    </section>
+  )
+}
+
+export default Home
