512db1f7b5a083a33fc5ccd5fa43f86108cd3170
Rishabh Sharma <41268157+rishabh-327@users.noreply.github.com>
2021-06-20 22:10:46 +0530

    Updated API data transformation logic.

--- src/views/Appointments/index.jsx
+++ src/views/Appointments/index.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { CornerDownLeft } from 'react-feather'
 import classnames from 'classnames'
+import { DateTime } from 'luxon'
 
 import AppSectionTitle from '../../components/AppSectionTitle'
 import VaccinationCenter from '../../components/VaccinationCenter'
@@ -11,7 +13,32 @@ import './Appointments.scss'
 import centersData from '../../data/centers'
 
 const Appointments = () => {
-  const centers = centersData
+  const [centers, setCenters] = useState([])
+
+  useEffect(() => {
+    setCenters(getDatewiseAppointments(centersData))
+  }, [])
+
+  const daywiseAppointments = centers.map((center, idx) => (
+    <>
+      <Row className="mb-5" key={idx}>
+        <Col xs="12">
+          <p className="mb-2">
+            <strong>{center.date}</strong>
+          </p>
+        </Col>
+
+        {center.centers.map(c => {
+          return (
+            <Col md="6" lg="4" key={c.center_id}>
+              <VaccinationCenter center={c} />
+            </Col>
+          )
+        })}
+      </Row>
+    </>
+  ))
+
   return (
     <div id="appointments-page">
       <div className="d-flex justify-content-between align-items-center mb-5">
@@ -23,20 +50,41 @@ const Appointments = () => {
         </button>
       </div>
 
-      <Row>
-        <Col xs="12">
-          <p className="mb-2">20 June 2021</p>
-        </Col>
-        {centers.map(center => {
-          return (
-            <Col md="6" lg="4" key={center.center_id}>
-              <VaccinationCenter center={center} />
-            </Col>
-          )
-        })}
-      </Row>
+      {daywiseAppointments}
     </div>
   )
 }
 
 export default Appointments
+
+const getDatewiseAppointments = centers => {
+  const datewiseCenters = {}
+
+  for (let center of centers) {
+    const datewiseSessions = {}
+
+    for (let session of center.sessions) {
+      if (!datewiseSessions[session.date]) datewiseSessions[session.date] = []
+      datewiseSessions[session.date].push(session)
+    }
+
+    for (let date in datewiseSessions) {
+      if (!datewiseCenters[date]) datewiseCenters[date] = []
+      datewiseCenters[date].push({
+        ...center,
+        sessions: [...datewiseSessions[date]],
+      })
+    }
+  }
+
+  const datewiseCentersList = []
+  for (let date in datewiseCenters) {
+    datewiseCentersList.push({
+      date: DateTime.fromFormat(date, 'dd-mm-yyyy').toFormat('d MMM yyyy, ccc'),
+      centers: datewiseCenters[date].sort(
+        (a, b) => a.sessions.length - b.sessions.length
+      ),
+    })
+  }
+  return datewiseCentersList
+}
