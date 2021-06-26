a3d47f21aa31c326bb85b6f3314cdfc964235e90
Rishabh Sharma <41268157+rishabh-327@users.noreply.github.com>
2021-06-26 15:21:12 +0530

    Footer cleanup.

--- src/components/AppFooter/index.jsx
+++ src/components/AppFooter/index.jsx
@@ -1,14 +1,27 @@
 import { Container, Row, Col } from 'react-bootstrap'
-import styles from './AppFooter.module.scss'
+import { GitHub } from 'react-feather'
+
+import _s from './AppFooter.module.scss'
 
 const AppFooter = () => {
   return (
-    <footer className={styles['appFooter']}>
+    <footer className={_s['appFooter']}>
       <Container className="h-100">
-        <Row className="justify-content-center align-items-center h-100">
-          <Col>
+        <Row className="justify-content-between align-items-center h-100">
+          {/* <Col>
             <p>
               Copyright &copy; 2021 <span className="app-title">vaccify</span>
+            </p>
+          </Col> */}
+          <Col className="text-center">
+            <p>
+              <a
+                href="https://github.com/rishabh-327/vaccify"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GitHub size="14" /> <span>Github</span>
+              </a>
             </p>
           </Col>
         </Row>
